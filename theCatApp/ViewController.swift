8979aa79432f458745d680fb86ed1f5f843d0a78
Julian Granada <jagranadab@gmail.com>
2020-09-05 12:08:29 -0500

    Initial Commit

---
+++ theCatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theCatApp
+//
+//  Created by Julián Granada on 5/09/20.
+//  Copyright © 2020 Julián Granada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
